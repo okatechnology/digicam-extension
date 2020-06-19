bcd0e6c6986c924fc927b70e01d051fa0cfbd3f2
okatechnology <tonkotsu6218@gmail.com>
2020-06-20 00:28:29 +0900

    updated pictures

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import './custom.css';
 import '../public/manifest.json';
 import '../public/background';
 import './additionalScripts/customTextarea';
-import '../assets/digicamExtensionLogo.png';
+import '../assets/digicamExtensionLogo128.png';
+import '../assets/digicamExtensionLogo48.png';
+import '../assets/digicamExtensionLogo16.png';
 
 // const buttonRoot = document.createElement('div');
 // document.body.appendChild(buttonRoot);
