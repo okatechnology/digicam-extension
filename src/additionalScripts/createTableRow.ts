9cbe71947f7dea402f8e0b6d17c772df6af80ccb
okatechnology <tonkotsu6218@gmail.com>
2020-07-01 16:02:16 +0900

    fixed new line bug

--- src/additionalScripts/createTableRow.ts
+++ src/additionalScripts/createTableRow.ts
@@ -19,7 +19,7 @@ export const createTableRow = (headerName: string, contents: string) => {
   textBox.classList.add(...textAreaClassNames);
 
   label.textContent = headerName;
-  textBox.innerHTML = contents.replace('\n', '<br />');
+  textBox.innerHTML = contents.replace(/\n/g, '<br />');
   textBox.style.lineHeight = 'normal';
   textBox.style.padding = '0 20px 0 0';
 
