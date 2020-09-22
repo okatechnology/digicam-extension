3e54dbb901e2afa4b3739fc912bd77923cc6cd33
okatechnology <tonkotsu6218@gmail.com>
2020-09-23 01:48:40 +0900

    add hide table function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './custom.css';
 import '../public/manifest.json';
 import '../public/background';
 import './additionalScripts/autoSaveAssignment';
+import './additionalScripts/hideTimetable/';
 import '../assets/digicamExtensionLogo128.png';
 import '../assets/digicamExtensionLogo48.png';
 import '../assets/digicamExtensionLogo16.png';
